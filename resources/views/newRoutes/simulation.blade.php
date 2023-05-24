8c5695415fff4a7550762ef2cc50b5883edaeb82
Andre Carpio <carpioandre7@gmail.com>
2023-05-23 23:56:00 -0400

    prueba de estructura de simulacion

--- resources/views/newRoutes/simulation.blade.php
+++ resources/views/newRoutes/simulation.blade.php
@@ -5,19 +5,53 @@
 @endsection
 
 @section('estilos')
-<link rel="stylesheet" href='{{ asset('css/style-simulacion.css') }}'>
+    <link rel="stylesheet" href='{{ asset('css/style-simulacion.css') }}'>
 @endsection
 
 @section('contenidoPrincipal')
+    <div class="contenedor-formulario-simulacion">
+        <p class="titulo-simulacion">Titulo de la simulacion ID: {{ $idSimulacion }}</p>
 
-   <div class="contenedor-formulario-simulacion">
-    <p class="titulo-simulacion">Titulo de la simulacion ID: {{$idSimulacion}}</p>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nombres y Apellidos (deudor/a):</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" >
+        </div>
 
-   </div>
+        <div class="row g-3 mb-3">
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Documento de Identidad No:</label>
+                <input type="text" class="form-control" aria-label="First name">
+            </div>
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Extensión</label>
+                <input type="text" class="form-control"  aria-label="Last name">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Edad:</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" >
+        </div>
+        <div class="row g-3 mb-3">
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Estado civil:</label>
+                <input type="text" class="form-control" aria-label="First name">
+            </div>
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Teléfono:</label>
+                <input type="text" class="form-control"  aria-label="Last name">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Dirección Domicilio (deudor):</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" >
+        </div>
 
+
+
+    </div>
 @endsection
 
 
 @section('scripts')
-<script src='{{ asset('js/script-simulacion.js') }}'></script>
+    <script src='{{ asset('js/script-simulacion.js') }}'></script>
 @endsection
--- storage/framework/views/d575a72811c9eed69caa10905ecdecaaf390a33a.php
+++ storage/framework/views/d575a72811c9eed69caa10905ecdecaaf390a33a.php
@@ -5,20 +5,55 @@
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('estilos'); ?>
-<link rel="stylesheet" href='<?php echo e(asset('css/style-simulacion.css')); ?>'>
+    <link rel="stylesheet" href='<?php echo e(asset('css/style-simulacion.css')); ?>'>
 <?php $__env->stopSection(); ?>
 
 <?php $__env->startSection('contenidoPrincipal'); ?>
+    <div class="contenedor-formulario-simulacion">
+        <p class="titulo-simulacion">Titulo de la simulacion ID: <?php echo e($idSimulacion); ?></p>
 
-   <div class="contenedor-formulario-simulacion">
-    <p class="titulo-simulacion">Titulo de la simulacion ID: <?php echo e($idSimulacion); ?></p>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Nombres y Apellidos (deudor/a):</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" >
+        </div>
 
-   </div>
+        <div class="row g-3 mb-3">
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Documento de Identidad No:</label>
+                <input type="text" class="form-control" aria-label="First name">
+            </div>
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Extensión</label>
+                <input type="text" class="form-control"  aria-label="Last name">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">Edad:</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" >
+        </div>
+        <div class="row g-3 mb-3">
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Estado civil:</label>
+                <input type="text" class="form-control" aria-label="First name">
+            </div>
+            <div class="col">
+                <label for="formGroupExampleInput" class="form-label">Teléfono:</label>
+                <input type="text" class="form-control"  aria-label="Last name">
+            </div>
+        </div>
+        <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">Dirección Domicilio (deudor):</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" >
+        </div>
 
+
+
+    </div>
 <?php $__env->stopSection(); ?>
 
 
 <?php $__env->startSection('scripts'); ?>
-<script src='<?php echo e(asset('js/script-simulacion.js')); ?>'></script>
+    <script src='<?php echo e(asset('js/script-simulacion.js')); ?>'></script>
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('templates.templateType1', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\proyectoSimulacion2023\resources\views/newRoutes/simulation.blade.php ENDPATH**/ ?>
